f896c34850d7c965a6e3f856cdd6eb73562d55bb
Rudra Pratap <rudrasikarwar.dev@gmail.com>
2026-06-22 08:58:58 +0530

    Implemented authentication repository

--- app/src/main/java/com/example/connecthub/MainActivity.kt
+++ app/src/main/java/com/example/connecthub/MainActivity.kt
@@ -5,14 +5,14 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.connecthub.firebase.FirebaseManager
+import com.example.connecthub.ui.auth.LoginScreen
+import com.example.connecthub.ui.auth.RegisterScreen
 import com.example.connecthub.ui.theme.ConnectHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,35 +22,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             ConnectHubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+
+                        LoginScreen(onRegisterClick = {})
+                    }
                 }
             }
         }
-        Log.d("FirebaseTest",
-            FirebaseManager.auth.toString()
-        )
 
-        Log.d("FirebaseTest",
-            FirebaseManager.firestore.toString()
-        )
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ConnectHubTheme {
-        Greeting("Android")
+        Log.d("FirebaseTest", FirebaseManager.auth.toString())
+        Log.d("FirebaseTest", FirebaseManager.firestore.toString())
     }
 }
---
+++ app/src/main/java/com/example/connecthub/data/repository/AuthRepository.kt
@@ -0,0 +1,46 @@
+package com.example.connecthub.data.repository
+
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthRepository {
+
+    private val auth = FirebaseAuth.getInstance()
+
+    fun registerUser(
+        email: String,
+        password: String,
+        onResult: (Boolean, String?) -> Unit
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onResult(true, null)
+                } else {
+                    onResult(false, task.exception?.message)
+                }
+            }
+    }
+
+
+    fun loginUser(
+        email: String,
+        password: String,
+        onResult: (Boolean, String?) -> Unit
+    ) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onResult(true, null)
+                } else {
+                    onResult(false, task.exception?.message)
+                }
+            }
+    }
+
+
+    fun logout() {
+        auth.signOut()
+    }
+
+    fun currentUser() = auth.currentUser
+}
