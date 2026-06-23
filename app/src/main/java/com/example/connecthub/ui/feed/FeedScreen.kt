e8bc8c8e9be6797ba6690bc3022bcca00b2de297
Rudra Pratap <rudrasikarwar.dev@gmail.com>
2026-06-23 15:54:04 +0530

    Added Feed screen

---
+++ app/src/main/java/com/example/connecthub/ui/feed/FeedScreen.kt
@@ -0,0 +1,33 @@
+package com.example.connecthub.ui.feed
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FeedScreen(
+    onLogoutClick: () -> Unit
+) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Welcome to ConnectHub"
+        )
+
+        Button(
+            onClick = onLogoutClick
+        ) {
+            Text("Logout")
+        }
+    }
+}
